3edeb9f3919f2258b17f4203db45cda831020771
YudhaBara <65260237+YudhaBara@users.noreply.github.com>
2020-05-13 06:38:57 +0700

    Add files via upload

---
+++ Yudhagans.sh
@@ -0,0 +1,9 @@
+# ! /system/bin/sh
+clear 
+echo "SELAMAT DATANG"
+echo "-Semoga hari anda menyenangkan-]"
+echo "--++[PROGRAM YUDHA SABARA]+--"
+sleep 5
+echo "PERCEPAT JARINGAN DI MULAI ! :)"
+sleep 3
+ping 8.8.8.8
